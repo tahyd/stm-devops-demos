db69e8caab55678c2ac315dc78ce9e0f309ca1b5
tahyd <ta.hyd.003@gmail.com>
2024-07-13 11:46:55 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/welcome")
+def welcome():
+    return "Welcome To Flask"
+@app.route('/login')
+def login() :
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
